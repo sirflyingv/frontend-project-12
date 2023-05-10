7dda7f6485137a56e1b97cf15158e7569ac737e1
sirflyingv <andthenyouadd@gmail.com>
2023-05-10 16:34:54 +0300

    form and auth validation styling

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable functional/no-expression-statements */
 /* eslint-disable import/no-extraneous-dependencies */
-import React /* , { useEffect, useState } */ from 'react';
+import React, { /* useEffect, */ useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Form, Container } from 'react-bootstrap';
+import {
+  Button, Form, Container, Alert,
+} from 'react-bootstrap';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+// import cn from 'classnames';
 import axios from 'axios';
 import { useAuth } from '../Contexts';
 
 const LoginForm = () => {
+  const [isAuthFailed, setAuthFailed] = useState(false);
   const navigate = useNavigate();
   const auth = useAuth();
   const { localStorage } = window;
@@ -28,17 +32,20 @@ const LoginForm = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-      }).catch((e) => console.log('op!', e));
-
+      }).catch((e) => {
+        setAuthFailed(true);
+        console.log('op!', e);
+      });
       localStorage.setItem('userId', JSON.stringify(res.data));
       auth.logIn();
+      setAuthFailed(false);
       navigate('/');
     },
   });
 
   return (
-    <Container className="container p-3 bg-light border rounded">
-      <Form onSubmit={formik.handleSubmit}>
+    <Container className="container bg-light border rounded">
+      <Form className="p-3" onSubmit={formik.handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
           <Form.Control
@@ -47,8 +54,13 @@ const LoginForm = () => {
             value={formik.values.username}
             onBlur={formik.handleBlur}
             type="text"
+            required
             placeholder="Enter name"
+            isInvalid={!!formik.errors.username}
           />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.username}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -59,13 +71,20 @@ const LoginForm = () => {
             value={formik.values.password}
             onBlur={formik.handleBlur}
             type="password"
+            required
             placeholder="Password"
+            isInvalid={!!formik.errors.password}
           />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.password}
+          </Form.Control.Feedback>
         </Form.Group>
+
         <Button variant="primary" type="submit">
           Log in
         </Button>
       </Form>
+      {isAuthFailed ? <Alert variant="danger">Wrong username or passwords</Alert> : null}
     </Container>
   );
 };
