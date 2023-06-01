8858afb8d10a0231a9d0e7e1c54bbbc817f843e0
sirflyingv <andthenyouadd@gmail.com>
2023-06-01 16:46:07 +0300

    routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,6 @@
+const baseUrl = '';
+const apiUrl = 'api/v1';
+
+export const dataUrl = [baseUrl, apiUrl, 'data'].join('/');
+export const loginUrl = [baseUrl, apiUrl, 'login'].join('/');
+export const signUpUrl = [baseUrl, apiUrl, 'signup'].join('/');
