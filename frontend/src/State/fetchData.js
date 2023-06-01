747510141b336f21898d096000c4dea4bcaf5c8e
sirflyingv <andthenyouadd@gmail.com>
2023-06-01 16:42:11 +0300

    routes

--- frontend/src/State/fetchData.js
+++ frontend/src/State/fetchData.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { dataUrl } from '../routes';
+import { dataUrl } from '../routes.js';
 
 const fetchData = createAsyncThunk(
   'data/fetchData',
